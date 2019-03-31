a7d50a5f070e12dd36edd44b25a9481773bdf897
Remigiusz Wilk <remekwilk@gmail.com>
2019-03-31 18:51:32 +0200

    adv

--- Moje_Projekty/Zaliczeniowe/moj_projekt.py
+++ Moje_Projekty/Zaliczeniowe/moj_projekt.py
@@ -1,34 +1,45 @@
 # Remigiusz Wilk
 from flask import Flask, request, render_template
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return render_template('input.html')
 
-@app.route('/odczyt')
+@app.route('/', methods=['get', 'post'])
+def welcome():
+    return render_template('welcome.html')
+
+
+@app.route('/wprowadz')
+def wprowadz():
+    return render_template('wprowadz.html')
+
+
+@app.route('/odczyt', methods=['get', 'post'])
 def odczyt_z_pliku():
-    argumenty = request.args
-    for klucz, wartosc in argumenty.items():
-        print(f'{klucz}: {wartosc}')
-    return f'argumenty: {argumenty}'
+    try:
+        with open('auta.json', mode='r') as plik:
+            dane = json.load(plik)
+            print(dane)
+            return render_template('zapis.html', dane=dane)
+    except FileNotFoundError:
+        return render_template('blad.html' , error='Plik nie istnieje')
 
-
-@app.route('/zapis')
+@app.route('/zapis', methods=['get', 'post'])
 def zapis_do_pliku():
+
     marka = request.args.get('Marka')
     model = request.args.get('Model')
-    with open('baza.txt', mode='a') as plik:
+    if marka and model:
+        lista_aut = []
+        wpis = {'Marka': marka, 'Model': model}
+        lista_aut.append(wpis)
 
-        if marka and model:
-            dane = model + marka
-            print(f'{marka}: {model}')
-            plik.write(dane)
-            return f'Dane samochodu: {dane}'
-        else:
-            return f'Brak danych do zapisu!'
-
+        with open('auta.json', mode='a') as plik:
+            json.dump(lista_aut, plik)
+            return render_template('zapis.html', **wpis)
+    else:
+        return render_template('blad.html', error='Nie wprowadzono auta')
 
 
 if __name__ == "__main__":
